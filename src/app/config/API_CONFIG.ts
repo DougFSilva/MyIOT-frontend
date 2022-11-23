68227c444e0108b6fe65720103038459f20411f6
DougFSilva <doug.ferreira.dfs@gmail.com>
2022-11-23 09:58:08 -0300

    Corrigido erro no css do componente home que impossibilitava a exibição da imagem no grid

--- src/app/config/API_CONFIG.ts
+++ src/app/config/API_CONFIG.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  baseUrl: 'http://localhost:8080',
-  //baseUrl: 'http://191.101.235.137:8080'
+  //baseUrl: 'http://localhost:8080',
+  baseUrl: 'http://191.101.235.137:8080'
 };
